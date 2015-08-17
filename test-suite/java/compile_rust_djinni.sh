3092aa2c7dc432c9b423182307338572a32ce73f
Danny Weinberg <danny@dropbox.com>
2015-08-17 15:11:49 -0700

    Add ability to compile and run Java unit tests with Rust as the companion language

---
+++ test-suite/java/compile_rust_djinni.sh
@@ -0,0 +1,12 @@
+
+
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+RUST_ROOT="$DIR/../handwritten-src/rust"
+
+pushd $"RUST_ROOT" > /dev/null
+
+cargo build --lib
+
+popd > /dev/null
+
+cp "$RUST_ROOT/target/debug/libdjinni_test_suite.dylib" $1
